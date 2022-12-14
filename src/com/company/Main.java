3a6d143085ddd63981f7139683d403b4a240674c
olegd <olegdubovoyy@gmail.com>
2022-12-14 15:38:16 +0300

    Writing vertices

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,40 +2,57 @@ package com.company;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
         Model m1 = new Model();
-        ArrayList<Vector3f> vertices = new ArrayList<Vector3f>();
-        Vector3f f1 = new Vector3f(2, 3, 3);
-        Vector3f f2 = new Vector3f(4, 6, 6);
-        Vector3f f3 = new Vector3f(4, 7, 6);
-        Vector3f f4 = new Vector3f(4, 8, 6);
-        Vector3f f5 = new Vector3f(4, 3, 6);
-
-        vertices.add(f1);
-        vertices.add(f2);
-        vertices.add(f3);
-        vertices.add(f4);
-        vertices.add(f5);
-        m1.vertices = vertices;
 
         String fileSeparator = System.getProperty("file.separator");
-        String pn = "E:" + fileSeparator + "textf3.obj";
+
+        //чтение файла
+        Path fileName = Path.of("../ObjModels/Faceform/WrapHand.obj");
+
+        Pattern pattern = Pattern.compile("[а-яё]");
+        Matcher matcher = pattern.matcher(fileName.toString().toLowerCase());
+        if (matcher.find()) {
+            throw new ObjReaderException("Can't read file with outsider symbols in its name", 1);
+        }
+
         try {
-            ObjWriter.createObjFile(pn);
+            String fileContent = Files.readString(fileName);
+            System.out.println("Loading model ...");
+            m1 = ObjReader.readModel(fileContent);
+
+            System.out.println("Vertices: " + m1.getVertices().size());
+            System.out.println("Texture vertices: " + m1.getTextureVertices().size());
+            System.out.println("Normals: " + m1.getNormals().size());
+            System.out.println("Polygons: " + m1.getPolygons().size());
+        } catch (IOException exception) {
+            throw new ObjReaderException("File not found", 1);
+        }
+
+        //запись файла
+        String filePath = "G:" + fileSeparator + "text5.obj";
+        try {
+            System.out.println("Создаём файл");
+            ObjWriter.createObjFile(filePath);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        File f = new File(pn);
-
+        File f = new File(filePath);
         try {
+            System.out.println("Запись в файл");
             ObjWriter.writeToFile(m1, f);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("Модель сохранена в файл");
     }
 }
--- src/com/company/ObjWriter.java
+++ src/com/company/ObjWriter.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.*;
+import java.text.DecimalFormat;
 
 public class ObjWriter {
 
@@ -17,16 +18,59 @@ public class ObjWriter {
     }
 
     public static void writeToFile(Model model, File file) throws IOException {
-        file.canWrite();
-        FileWriter fw = new FileWriter(file, false);
         String str = "";
-        for (int i = 0; i < model.vertices.size(); i++){
-            str = str + "v " + String.valueOf(model.vertices.get(i).x) + " " +
-                    String.valueOf(model.vertices.get(i).y) + " " +
-                    String.valueOf(model.vertices.get(i).z) + "\n";
-           toFile(str, file.getAbsolutePath());
-        }
 
+        str = writeVertexes(str, model);
+        str = writeTextureVertexes(str, model);
+        str = writePolygons(str, model);
 
+        toFile(str, file.getAbsolutePath());
     }
+
+    private static String writeVertexes(String str, Model model){
+        for (int i = 0; i < model.vertices.size(); i++){
+            final String vx = String.format("%.4f", model.vertices.get(i).x).replace(',', '.');
+            final String vy = String.format("%.4f", model.vertices.get(i).y).replace(',', '.');
+            final String vz = String.format("%.4f", model.vertices.get(i).z).replace(',', '.');
+            str = str + "v  " + vx + " " + vy + " " + vz + "\n";
+            System.out.println("Т " + i);
+        }
+        System.out.println("Точки");
+        str = str + "# " + model.vertices.size() + " vertices";
+        str+="\n";
+        str+="\n";
+        return str;
+    }
+
+    private static String writeTextureVertexes(String str, Model model){
+        for (int i = 0; i < model.textureVertices.size(); i++){
+            final String vtx = String.format("%.4f", model.textureVertices.get(i).x).replace(',', '.');
+            final String vty = String.format("%.4f", model.textureVertices.get(i).y).replace(',', '.');
+            str = str + "vt " + vtx + " " + vty + " " + "0.0000" + "\n";
+            System.out.println("Ткс " + i);
+        }
+        System.out.println("Текстуры");
+        str = str + "# " + model.textureVertices.size() + " texture coords";
+        str+="\n";
+        str+="\n";
+        return str;
+    }
+
+    private static String writePolygons(String str, Model model){
+        for (int i = 0; i < model.polygons.size(); i++){
+            str = str + "f ";
+            for (int j = 0; j < model.polygons.get(i).getVertexIndices().size(); j++){
+                str = str  + String.valueOf(model.polygons.get(i).getVertexIndices().get(j) + 1) + "/"
+                        + String.valueOf(model.polygons.get(i).getTextureVertexIndices().get(j) + 1) + " ";
+            }
+            str = str  + "\n";
+            System.out.println("П " + i);
+        }
+        str = str + "# " + model.polygons.size() + " polygons";
+        str+="\n";
+        str+="\n";
+        System.out.println("Полигоны");
+        return str;
+    }
+
 }
